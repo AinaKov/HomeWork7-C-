ec5ab0f85b2cb607d359d568a8c057ca15a9e357
AlinaKov <alkovkova@gmail.com>
2022-09-05 02:25:58 +0500

    Added Task47

---
+++ Task47/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9 */
+
+void PrintArray(double[,] tab)
+{
+    for (int i = 0; i < tab.GetLength(0); i++)
+    {
+        for (int j = 0; j < tab.GetLength(1); j++)
+        {
+            Console.Write($"{tab[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] tab)
+{
+    for (int i = 0; i < tab.GetLength(0); i++)
+    {
+        for (int j = 0; j < tab.GetLength(1); j++)
+        {
+
+            tab[i,j] = new Random().Next(-1000,1000)/10.0;
+        }
+    }
+}
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] table = new double[m, n];
+
+FillArray(table);
+PrintArray(table);
+
+
